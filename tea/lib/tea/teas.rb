72df9dad9be0fc88a8f9b185dc13223299924bd9
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-25 21:28:58 +0000

    updated get_url in Importer

--- tea/lib/tea/teas.rb
+++ tea/lib/tea/teas.rb
@@ -3,46 +3,32 @@ class Teas
 
   @@all = []
 
-  def initialize(type, name, info, url)
-    @type = type
-    @name = name
-    @info = info
-    @url = url
+  def initialize
     @@all << self
-    #puts "#{self}, #{self.name}, #{self.type}, #{self.info}, #{self.url}"
   end
 
   def self.all
     @@all
   end
 
-  #def self.obj_create_g
-    #puts "calls obj create"
-    #type = "green"
-    #name = "Gunpowder"
-    #time = 120
-    #temp = 170
-    #puts "Type: #{type}, Name: #{name}, Time: #{time}, Temp: #{temp}"
-    #self.new(type, name, time, temp)
-  #end
-
-  #def self.obj_create_b
-    ##type = "black"
-    #name = "ruby"
-    #time = 150
-    #temp = 200
-    #puts "Type: #{type}, Name: #{name}, Time: #{time}, Temp: #{temp}"
-    #new(type, name, time, temp)
-  #end
-
   def self.a_to_z
-    puts "calls a to z"
+    puts "Importing all teas"
+    Importer.scrape_teas("all")
     az = @@all.sort_by {|obj| obj.name}
-    az.each.with_index(1) { |t, i| puts "  #{i}. #{t.name}"}
+    az.each.with_index(1) do |t, i|
+      puts "#{i}. #{t.name}"
+      puts "      #{t.type}"
+      puts "      #{t.info}"
+      puts "--------------------------"
   end
 
-  def self.list_by_tea_type(type)
-    puts "calls lists by tea type"
-    Importer.scrape_teas(type)
+  def self.list_by_tea_type(oxidation)
+    puts "Importing #{oxidation} teas"
+    Importer.scrape_teas(oxidation)
+    az = @@all.collect {|obj| obj.type == oxidation}
+    az.each.with_index(1) do |t, i|
+    puts "#{i}. #{t.name}"
+    puts "      #{t.info}"
+    puts "--------------------------"
   end
 end
