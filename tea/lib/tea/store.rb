11906af64bc36e5fcc819f09d8c2d88d73e92939
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-25 18:39:21 +0000

    varified scrape links for attributes

--- tea/lib/tea/store.rb
+++ tea/lib/tea/store.rb
@@ -20,17 +20,14 @@ class Importer
 
   def self.scrape_teas
       puts "Here's a list of the teas you asked for."
-      arr = []
-      doc = Nokogiri::HTML(open("https://meileaf.com/teas/"))
-      tea = doc.css('div.product-card__info ') #.each do |tea|
-      #arr << {
-        name = tea.css('div.product-card__info-main h3').text
-        info = tea.css('div.product-card__info-main p').text
-        type = tea.css('span.product-card__type').text
-        url = tea.css('div.product-card__info h2')['href'].text
-        url_f = tea.css('a.product-card__image-link').first['href']
-      #}
-      binding.pry
-    #end
+      doc = Nokogiri::HTML(open("https://meileaf.com/teas"))
+      tea = doc.css('div.product-card__info')
+        name = tea.css('h3').first.text
+        info = tea.css('div.product-card__info-main p').first.text
+        type = tea.css('span.product-card__type').first.text
+        url = "https://meileaf.com" + tea.css('div.product-card__info-top a').first['href']
+      binding.pry  #url_f = tea.css('a.product-card__image-link')['href']
+
+        # tea.css('h3').first.text
   end
 end
