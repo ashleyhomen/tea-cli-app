59548dd962c17dcd31e5bce17d1bb3d6ed8589ed
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-25 18:58:41 +0000

    added each to scrape_teas method

--- tea/lib/tea/store.rb
+++ tea/lib/tea/store.rb
@@ -21,13 +21,14 @@ class Importer
   def self.scrape_teas
       puts "Here's a list of the teas you asked for."
       doc = Nokogiri::HTML(open("https://meileaf.com/teas"))
-      tea = doc.css('div.product-card__info')
-        name = tea.css('h3').first.text
-        info = tea.css('div.product-card__info-main p').first.text
-        type = tea.css('span.product-card__type').first.text
-        url = "https://meileaf.com" + tea.css('div.product-card__info-top a').first['href']
-      binding.pry  #url_f = tea.css('a.product-card__image-link')['href']
+      doc.css('div.product-card__info').each do |tea|
+        name = tea.css('h3').text
+        info = tea.css('div.product-card__info-main p').text
+        type = tea.css('span.product-card__type').text
+        url = "https://meileaf.com#{tea.css('div.product-card__info-top a')['href']}"
+        Tea.new(type, name, info, url)
+          binding.pry
+      end
 
-        # tea.css('h3').first.text
   end
 end
