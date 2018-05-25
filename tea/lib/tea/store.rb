fa18e27e35cc537e29f6c6ba0a1651c710770a04
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-25 03:57:05 +0000

    testing each structure for scrape

--- tea/lib/tea/store.rb
+++ tea/lib/tea/store.rb
@@ -19,14 +19,18 @@ class Importer
   end
 
   def self.scrape_teas
-    puts "Here's a list of the teas you asked for."
-    #url = get_url(type)
-    doc = Nokogiri::HTML(open("https://meileaf.com/teas/"))
-    binding.pry
-    t = doc.css('div.container product-index')
-    name = t.css('div.product-card__info-main h3').first.text
-    info = doc.css('div.product-card__info-main p').first.text
-    info = doc.css('span.product-card__type').first.text
-    url = doc.css('a.product-card__image-link').first['href']
+      puts "Here's a list of the teas you asked for."
+      arr = []
+      doc = Nokogiri::HTML(open("https://meileaf.com/teas/"))
+      tea = doc.css('div.product-card__info ') #.each do |tea|
+      #arr << {
+        name = tea.css('div.product-card__info-main h3').text
+        info = tea.css('div.product-card__info-main p').text
+        type = tea.css('span.product-card__type').text
+        url = tea.css('div.product-card__info h2')['href'].text
+        url_f = tea.css('a.product-card__image-link').first['href']
+      #}
+      binding.pry
+    #end
   end
 end
