c3dba0d37cee0f62eeb0f369239bd39bb65d19ea
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-27 21:14:02 +0000

    moved menus to module

--- tea/lib/tea/cli.rb
+++ tea/lib/tea/cli.rb
@@ -1,10 +1,12 @@
+require_relative './concerns'
 class Session
-
+  include Concerns::Menus
   attr_accessor :name
 
   def call
     greeting
-    menu
+    hub
+    goodbye
   end
 
   def greeting
@@ -15,28 +17,28 @@ class Session
     puts "   Hello #{@name}"
   end
 
-  def menu
-    self.class.request_input("number")
+  def hub
+    request_input("number")
     puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    self.class.main_menu
+    main_menu
     input = nil
     Importer.scrape_teas
     while input != "exit"
       input = gets.strip.to_s.downcase
       if input == "menu"
-        self.class.main_menu
+        main_menu
       elsif input == "1"
         Teas.list_a_to_z
       elsif input == "2"
-        submenu(Teas::TEA_TYPES, "type")
+        type_submenu
       elsif input == "3"
-        submenu(Teas::COUNTRIES, "country")
+        country_submenu
       elsif input == "exit"
-        goodbye
+        nil
       else
         puts "sorry, I didn't get that..."
         request_input("number")
-        self.class.main_menu
+        main_menu
       end
     end
   end
@@ -45,33 +47,6 @@ class Session
     puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
     puts "   Goodbye #{@name}!"
     puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-  end
-
-  def submenu(list, item_type)
-    input = nil
-    until valid_input?(list, input)
-      self.class.request_input(item_type)
-      list.each {|item| puts item}
-      input = gets.strip.capitalize
-    end
-    Teas.list_by(item_type, input)
-  end
-
-  def valid_input?(list, input)
-    list.include?(input)
-    binding.pry
-  end
-
-  def self.main_menu
-    puts ""
-    puts "   1. List all teas"
-    puts "   2. List teas by type"
-    puts "   3. List teas by country"
-    puts "   To exit this sisseion enter: exit"
-  end
-
-  def self.request_input(item_type)
-    puts "   Please enter a #{item_type} from the list."
   end
 end
 #  ./bin/teahead
--- tea/lib/tea/teas.rb
+++ tea/lib/tea/teas.rb
@@ -1,4 +1,5 @@
 class Teas
+  extend Concerns::Menus
   attr_accessor :name, :aka, :type, :info, :url, :notes, :gongfu_instructions, :western_instructions
   TEA_TYPES = ["White", "Green", "Yellow", "Oolong", "Black", "Puerh"]
   COUNTRIES = ["China", "Japan", "Taiwan"]
@@ -25,7 +26,7 @@ class Teas
   end
 
   def self.list_by_country(input)
-    tea_array = self.all.collect { |tea| tea.notes.include?(input.capitalize)}
+    tea_array = self.all.select { |tea| tea.notes.include?(input.capitalize)}
     tea_array.each.with_index(1) { |t, i| print_tea_card(t, i)}
     learn_more(tea_array)
   end
@@ -45,7 +46,7 @@ class Teas
     puts "To return to the main menu enter: menu"
     puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
     input = nil
-    while input != "menu"
+    while input != "menu" && input != "exit"
     input = gets.strip
       if (1..array.size) === input.to_i
         Importer.scrape_tea_profile(array[input.to_i - 1])
@@ -54,7 +55,7 @@ class Teas
         puts "To see another profile enter: 1-#{array.size}"
         puts "To return to the main menu enter: menu"
         puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-      elsif input == "menu"
+      elsif input == "menu" || input == "exit"
         nil
       else
         puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
@@ -63,7 +64,7 @@ class Teas
         puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
       end
     end
-    Session.list_options
+    menu
   end
 
   def self.print_tea_profile(obj)
@@ -83,8 +84,7 @@ class Teas
     end
     puts ""
     puts "    GONGFU STEEPING INSTRUCTIONS:"
-    obj.gongfu_instructions.each { |k, v| puts "       #{k.to_s.split("_").join(" ")}: #{v}"
-  binding.pry}
+    obj.gongfu_instructions.each { |k, v| puts "       #{k.to_s.split("_").join(" ")}: #{v}"}
     puts ""
     puts "    WESTERN STEEPING INSTRUCTIONS:"
     obj.western_instructions.each { |k, v| puts "       #{k.to_s.split("_").join(" ")}: #{v}"}
